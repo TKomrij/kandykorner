a6b652af014d11108eb2d5a64ed8146b56d5a6d6
TKomrij <tatiane.b.komrij@gmail.com>
2021-01-27 15:52:11 -0600

    add product type to products

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -6,6 +6,6 @@ export const ProductCard = ({product}) => (
         <h3 className="product__id">Product ID: {product.id}</h3>
         <div className="product__name">Name: {product.name}</div>
         <div className="product__price">Price: {product.price}</div>
-        <div className="product__typeId">Type ID: {product.typeId}</div>
+        <div className="product__typeId">Type: {product.productType.type}</div>
     </section>
 )
